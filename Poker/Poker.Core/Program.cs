8eba5f16478d70180ef7ce2ab47c149f8203a664
Stefan Lindberg <stefan@gong.se>
2020-03-28 10:35:19 +0100

    Init

---
+++ Poker/Poker.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poker.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
